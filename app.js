291c68b3b6aaf2139b95f65768f5542f4770ba52
gfxavawilson <gfx.avawilson@gmail.com>
2023-08-13 17:30:46 +0500

    practice 5 chps

---
+++ app.js
@@ -0,0 +1,61 @@
+/// Class 1
+
+// console.log("hello world")
+// alert("Alert")
+
+// var userName = prompt("What is your name?");
+// console.log("Your name is " + userName)
+// document.write('Your name is ' + userName)
+
+
+///// ---> VARIABLES
+// Data types 1-String 2-Boolean 3-Number
+var age = 2;
+var age2 = 2;
+var isOnline = true;
+// console.log(isOnline)
+const name = "Saqlain";
+// name = "Salamat" // not changeable due to const 
+// console.log(name)
+let a; // declaration
+a = 100 // initialization
+console.log(a)
+
+// const bookName; //const must be initialized 
+// bookName = 'aaa'
+var b = 10;
+let c = 2;
+console.log(b)
+console.log(c)
+
+
+///// ---> Rules
+// 1- A variable name can't contain any spaces
+// var var2 aaa = 'fff' 
+
+// 2- A variable name can contain only letters, numbers, dollar signs, and underscores.
+var var3$_a = 'aaaaaaaaaa'
+console.log(var3$_a)
+
+// 3-  variable name can't be any of JavaScript's keywords, 
+// var var = 'aaa' //error
+// var const = 'aaa' //error
+// var let = 'aaa' //error
+
+
+// var School = "Alqalam"
+// console.log(School)
+
+
+
+/// ---> Math expression
+var num1 = 10
+var num2 = 15
+var result = num2 / num1
+console.log(result)
+console.log(1 + (2 - 3) * 2)
+
+
+// alert, console , prompt, document.write
+// Variables, data types, rules
+// Math expressions
