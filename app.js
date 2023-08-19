128fb22d3dc5e472976ec7c190c3d263435c15eb
gfxavawilson <gfx.avawilson@gmail.com>
2023-08-19 15:40:38 +0500

    chp 11 done

--- app.js
+++ app.js
@@ -1,68 +1,99 @@
-/// Class 1
+// /// Class 1
 
-// console.log("hello world")
-// alert("Alert")
+// // console.log("hello world")
+// // alert("Alert")
 
-// var userName = prompt("What is your name?");
-// console.log("Your name is " + userName)
-// document.write('Your name is ' + userName)
-
-
-///// ---> VARIABLES
-// Data types 1-String 2-Boolean 3-Number
-var age = 2;
-var age2 = 2;
-var isOnline = true;
-// console.log(isOnline)
-const name = "Saqlain";
-// name = "Salamat" // not changeable due to const 
-// console.log(name)
-let a; // declaration
-a = 100 // initialization
-console.log(a)
-
-// const bookName; //const must be initialized 
-// bookName = 'aaa'
-var b = 10;
-let c = 2;
-console.log(b)
-console.log(c)
+// // var userName = prompt("What is your name?");
+// // console.log("Your name is " + userName)
+// // document.write('Your name is ' + userName)
 
 
-///// ---> Rules
-// 1- A variable name can't contain any spaces
-// var var2 aaa = 'fff' 
+// ///// ---> VARIABLES
+// // Data types 1-String 2-Boolean 3-Number
+// var age = 2;
+// var age2 = 2;
+// var isOnline = true;
+// // console.log(isOnline)
+// const name = "Saqlain";
+// // name = "Salamat" // not changeable due to const 
+// // console.log(name)
+// let a; // declaration
+// a = 100 // initialization
+// console.log(a)
 
-// 2- A variable name can contain only letters, numbers, dollar signs, and underscores.
-var var3$_a = 'aaaaaaaaaa'
-console.log(var3$_a)
-
-// 3-  variable name can't be any of JavaScript's keywords, 
-// var var = 'aaa' //error
-// var const = 'aaa' //error
-// var let = 'aaa' //error
-
-
-// var School = "Alqalam"
-// console.log(School)
-
-
-
-/// ---> Math expression
-var num1 = 10
-var num2 = 15
-var result = num2 / num1
-console.log(result)
-console.log(1 + (2 - 3) * 2)
+// // const bookName; //const must be initialized 
+// // bookName = 'aaa'
+// var b = 10;
+// let c = 2;
+// console.log(b)
+// console.log(c)
 
 
-// alert, console , prompt, document.write
-// Variables, data types, rules
-// Math expressions
+// ///// ---> Rules
+// // 1- A variable name can't contain any spaces
+// // var var2 aaa = 'fff' 
+
+// // 2- A variable name can contain only letters, numbers, dollar signs, and underscores.
+// var var3$_a = 'aaaaaaaaaa'
+// console.log(var3$_a)
+
+// // 3-  variable name can't be any of JavaScript's keywords, 
+// // var var = 'aaa' //error
+// // var const = 'aaa' //error
+// // var let = 'aaa' //error
+
+
+// // var School = "Alqalam"
+// // console.log(School)
 
 
 
-// Push code on github Steps
-// git add .
-// git commit -m "message"
-// git push
+// /// ---> Math expression
+// var num1 = 10
+// var num2 = 15
+// var result = num2 / num1
+// console.log(result)
+// console.log(1 + (2 - 3) * 2)
+
+
+// // alert, console , prompt, document.write
+// // Variables, data types, rules
+// // Math expressions
+
+
+
+// // Push code on github Steps
+// // git add .
+// // git commit -m "message"
+// // git push
+
+
+
+//// STRING CONCATENATION 
+var name = "Ali"
+var school = "Alqalam"
+var classGrade = 10;
+// alert("Student name is "+name + " He studies at "+ school + " in class " + classGrade)
+
+
+//// PROMT 
+// prompt("Your school is ", school)
+
+
+////IF_ELSE
+var marks = "60";
+// if(marks >= 50){
+//     alert("You are Pass!")
+// }else{  
+//     alert("You are Fail!")
+// }
+// console.log(typeof marks    )
+//// Comparison operators
+// === checks type also 
+// if (marks !== 60) {
+//     console.log("true")
+// } else {
+//     console.log("false")
+// }
+
+console.log(!true)
